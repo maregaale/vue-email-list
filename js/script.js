8f79aeccc952b8c7137644a70bb7ef7c10e0419b
Alessandro Marega <ale_marega@hotmail.it>
2021-04-07 15:46:53 +0200

    aggiunta cdn axios, stampate a schermo le mail generate random

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ const app = new Vue (
   {
     el: "#root",
     data: {
-
+      mailingList: [],
+    },
+    mounted: function () {
+      for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((mail) => {
+          this.mailingList.push(mail.data.response);
+        });
+      }
     },
   }
 );
